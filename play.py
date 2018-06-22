7aa749d41079b7470e1a95e6fc6c292b62f4e793
Baldribert <baldribert1@gmail.com>
2018-06-22 16:31:52 +1000

    Deleted the TextFace for Nodes

--- play.py
+++ play.py
@@ -11,7 +11,7 @@ def get_example_tree():
 
     # delete nodes supported less than 70% in Bootstrap
     for node in t.get_descendants():
-        if not node.is_leaf() and node.support <= 70:
+        if not node.is_leaf() and node.support <= 90:
             node.delete()
     #set size to 0
 
@@ -47,67 +47,13 @@ def get_example_tree():
     n4.set_style(nst4)
 
 
-    #TextFace for Raffaelea sulphurea
-
-    Raff_Sulphurea_complex = TextFace("Raffaelea sulphurea")
-    Raff_Sulphurea_complex.margin_left = 100
-    Raff_Sulphurea_complex.margin_bottom = 20
-    Raff_Sulphurea_complex.fsize = 15
-    Raff_Sulphurea_complex.fstyle = "italic"
-
-    complex_text = TextFace("complex")
-    complex_text.fsize = 15
-    complex_text.margin_left = 15
-    complex_text.margin_bottom = 20
-
-    #Textface for Raffaelea lauricola
-
-    Raff_Lauri_complex = TextFace("Raffaelea lauricola")
-    Raff_Lauri_complex.margin_left = 200
-    Raff_Lauri_complex.margin_top = 20
-    Raff_Lauri_complex.fsize = 15
-    Raff_Lauri_complex.fstyle = "italic"
-
-    complex_text_l = TextFace("complex")
-    complex_text_l.fsize = 15
-    complex_text_l.margin_left = 15
-    complex_text_l.margin_top = 20
-
-    # Textface for Raffaelea lauricola
-
-    Raff_Aus = TextFace("Raffaelea")
-    Raff_Aus.margin_left = 5
-    Raff_Aus.fsize = 15
-    Raff_Aus.fstyle = "italic"
-
-
-    Aus = TextFace("Australian Clade of ")
-    Aus.fsize = 15
-    Aus.margin_left = 100
-
-
-
-    # Where are the different Raffaelea complex
-    sulphurea = t & ("Raffaelea_sulphurea")
-    sulphurea.add_face(complex_text, column=2, position="branch-right")
-    sulphurea.add_face(Raff_Sulphurea_complex,column=1, position="branch-right")
-    #sulphurea.add_face(Raff_Ai, column=1, position = "branch-right")
-
-    lauricola = t & ("Raffaelea_lauricola")
-    lauricola.add_face(Raff_Lauri_complex, column=1, position = "branch-right")
-    lauricola.add_face(complex_text_l, column=2, position = "branch-right")
-
-    Raff_kentii = t & ("CMW50200_Raffaelea_kentii_sp._nov")
-    Raff_kentii.add_face(Aus, column=1, position = "branch-right")
-    Raff_kentii.add_face(Raff_Aus, column=2, position = "branch-right")
-
 
 
 
 
     # Set the path in which images are located
-    img_path = "./"
-    # Raff_Ai = faces.ImgFace(img_path + "Fig2_CMW49901.png")
+    #img_path = "./"
+    #Raff_Ai = faces.ImgFace(img_path + "Fig2_CMW49901.png")
 
     # Specify the boundaries of the Image, how big do you want it
     # Raff_Ai.width = 200
@@ -131,8 +77,8 @@ def get_example_tree():
 
 def scientific_name_face(node, *args, **kwargs):
     scientific_name_text = QGraphicsTextItem()
-    underscore = node.name.replace("_", " ")
-    words = underscore.split()
+    #underscore = node.name.replace("_", " ")
+    words = node.name.split("_")
     text = []
     if len(words) < 2:
         # some sort of acronym or bin name, leave it alone
@@ -182,7 +128,7 @@ def layout(node):
         #faces.add_face_to_node(AttrFace("name", fstyle="italic"), node, column=0, )
         F = faces.DynamicItemFace(scientific_name_face)
         F.margin_left = 10
-        faces.add_face_to_node(F, node, 0)
+        faces.add_face_to_node(F, node, 0,)
 
 
 if __name__ == "__main__":
